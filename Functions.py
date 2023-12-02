99a2649a635fa5bb5639dd3ea09409e438bf72fc
erixh <ggeese67@gmail.com>
2023-12-02 17:05:08 -0600

    I made a discrepancy function -Erix H  test logic to see if this is what you want

--- Functions.py
+++ Functions.py
@@ -23,6 +23,27 @@ def ev_calc(pref_rate,opp_rate,bet_amt):
     ev = winning - losses
     return ev
 
+# function to find discrepancy between what might be true probability vs bookmaker odds
+def adj_prob(pref_rate,opp_rate):
+    # checks if rate we play is negative, converts to implied probability accordingly
+    if pref_rate < 0: 
+        reciprocal = pref_rate * -1
+        implied_pref = reciprocal / (reciprocal+100)
+    else:
+        implied_pref = 100 / (pref_rate+100)
+    # checks if rate against us is negative, converts to implied probability accordingly
+    if opp_rate < 0:
+        opp_reciprocal = opp_rate * -1
+        implied_opp = opp_reciprocal / (opp_reciprocal +100)
+    else:
+        implied_opp = 100 / (100 + opp_rate)
+    # creates adjusted probability
+    sum_prob = implied_opp + implied_pref
+    adjusted_pref = implied_pref / sum_prob
+    adjusted_opp = implied_opp / sum_prob
+    return (f"The discrepancy between favorites is adjusted: {adjusted_pref:.2f} - implied {implied_pref:.2f} = {adjusted_pref-implied_pref:.2f} and the discrepancy between the underdogs is adjusted: {adjusted_opp:.2f} - implied: {implied_opp:.2f} = {adjusted_opp-implied_opp:.2f}")
+
+    
 
 
     

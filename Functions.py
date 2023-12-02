db3a4a8dba2c3598b982cd7a8bd428b3e390fa0c
erixh <ggeese67@gmail.com>
2023-12-01 21:47:45 -0600

    This is a simple calculator for EV  This is meant to be used for hitting on single lines only!

---
+++ Functions.py
@@ -0,0 +1,27 @@
+
+def ev_calc(pref_rate,opp_rate,bet_amt):
+    if pref_rate < 0:
+        pref_odds1 = pref_rate *-1 
+        pref_odds = pref_rate*-1
+        pref_odds = pref_odds/(pref_odds+100)
+        payout = bet_amt * (100/pref_odds1)
+        winning = payout * pref_odds 
+    else:
+        pref_odds = 100 / (pref_rate+100)
+        payout = bet_amt * (pref_rate / 100)
+        winning = payout * pref_odds
+    if opp_rate < 0: 
+        opp_odds1 = opp_rate *-1
+        opp_odds = opp_rate * - 1
+        opp_odds = opp_odds / (opp_odds+100)
+        losses = bet_amt * opp_odds
+
+
+    else:
+        opp_odds = 100 / (opp_rate+100)
+        losses = bet_amt * opp_odds
+    ev = winning / losses
+    return ev
+
+    
+    

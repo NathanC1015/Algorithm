ea3a36a8d4ac29ffc8e2bc35dd34fc7417e9133c
Nathan <NathanCo2021@gmail.com>
2023-12-01 21:16:41 -0600

    Basic Calculator This is the basic calculator we will use for now -Nathan Co

--- Calculator.py
+++ Calculator.py
@@ -1,9 +1,28 @@
 import Testing_math as tm
 
 def Calc():
-    legs = tm.leg_count()
+    checker = "Y"
+    counter = 0
+
     evs = []
     names = []
+    pref_rate = []
+    opp_rate = []
 
-    for i in range():
+    while(checker == "Y"):
+        names[counter] = str(input("Input the potential leg : "))
+        pref_rate[counter] = float(input("Input the preferred rate : "))
+        opp_rate[counter] = float(input("Input the opposition rate : "))
+        counter += 1
+        checker = str(input("Input Y or N : "))
+
+    for i in range(counter + 1):
+        evs[i] = tm.ev_calc(pref_rate, opp_rate)
+
+    for i in range(evs):
+        print(names[i] + " : " evs[i])
+
+        
+
+
 
